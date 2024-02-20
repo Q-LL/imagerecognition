fce0b81370e2e25db133c8482a241739f91deabe
QLL-LIN <qll-lin@qq.com>
2024-02-20 11:31:48 +0800

    first updata

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,23 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
+
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const {ElementPlusResolver} = require('unplugin-vue-components/resolvers')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+    configureWebpack: (config) => {
+        if (!config.plugins) {
+            config.plugins = []
+        }
+        config.plugins.push(
+            AutoImport({
+                resolvers: [ElementPlusResolver()]
+            })
+        )
+        config.plugins.push(
+            Components({
+                resolvers: [ElementPlusResolver()]
+            })
+        )
+    }
 })
