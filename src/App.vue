e7a71716f469c2e736a216790217ae9e41d3da8f
QLL-LIN <qll-lin@qq.com>
2024-03-07 00:30:08 +0800

    尝试引入多语言支持

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <template>
   <div class="header">
-    <router-link to="/">主页</router-link> |
-    <router-link to="/start">开始使用</router-link> |
-    <router-link to="/about">关于项目</router-link>
+    <el-row style="margin: auto; width: 180px;">
+      <router-link to="/">主页</router-link> |
+      <router-link to="/start">开始使用</router-link> |
+      <router-link to="/about">关于项目</router-link>
+    </el-row>
+    <el-row style="margin-left: 90%; padding: 0;">
+      <el-dropdown>
+        <el-button type="primary">
+          选择语言<el-icon class="el-icon--right">
+            <ArrowDownBold />
+          </el-icon>
+        </el-button>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item>中文</el-dropdown-item>
+            <el-dropdown-item>English</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </el-row>
+
   </div>
   <div>
-    <router-view/>
+    <router-view />
   </div>
   <div class="footer">
 
@@ -35,15 +53,14 @@ window.ResizeObserver = class ResizeObserver extends _ResizeObserver {
 </script>
 
 <style>
-*{
-	margin:0;
-	padding:0;
-	border:0
+* {
+  margin: 0;
+  padding: 0;
+  border: 0
 }
 
-.header{
-  text-align: center;
-  background-color: #0095ff;
+.header {
+  background-color: #81c5f6be;
   width: 100vw;
   height: 4vh;
   padding-top: 1vh;
--- src/main.js
+++ src/main.js
@@ -3,5 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'element-plus/theme-chalk/dark/css-vars.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import i18n from './i18n'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+// createApp(App).use(store).use(router).mount('#app')
+app.use(i18n);
+app.use(store);
+app.use(router);
+app.mount('#app')
